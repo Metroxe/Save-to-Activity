358a1fe62932325d1ee1e8a3152b2d7ae23c4b02
Metroxe <chrispow96@gmail.com>
2016-09-01 10:35:45 -0700

    Added Sample

---
+++ sample/src/main/java/powroznik/christopher/sample/MainActivity.java
@@ -0,0 +1,94 @@
+package powroznik.christopher.sample;
+
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.HashMap;
+
+import powroznik.christopher.save_to_activity.SaveToActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    SaveToActivity saveToActivity = new SaveToActivity();
+    HashMap<String, String> hashMap = saveToActivity.StartHashMap();
+
+    public void SaveInformation(HashMap<String, String> newHashMap) {
+        hashMap = newHashMap;
+    }
+
+    public HashMap<String, String> LoadInformation() {
+        return hashMap;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final EditText putKey = (EditText) findViewById(R.id.putKey);
+        final EditText putString = (EditText) findViewById(R.id.putString);
+        final EditText getKey = (EditText) findViewById(R.id.getKey);
+        final TextView getString = (TextView) findViewById(R.id.getValue);
+        Button putButton = (Button) findViewById(R.id.put);
+        Button getButton = (Button) findViewById(R.id.get);
+        Button openFragment = (Button) findViewById(R.id.openFragment);
+
+        //put
+        if (putButton != null && putKey != null && putString != null) {
+            putButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String key = putKey.getText().toString();
+                    String string = putString.getText().toString();
+                    if (!key.isEmpty() && !string.isEmpty()) {
+                        try {
+                            saveToActivity.PutSingleString(MainActivity.this, key, string);
+                        } catch (ClassNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+        }
+
+        //get
+        if (getButton != null && getKey != null && getString != null) {
+            getButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String key = getKey.getText().toString();
+                    if (!key.isEmpty()) {
+                        try {
+                            getString.setText(saveToActivity.GetSingleHashString(MainActivity.this, key));
+                        } catch (ClassNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+        }
+
+        //Open Fragment
+        if (openFragment != null) {
+            openFragment.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    SingleStringFragment singleStringFragment = new SingleStringFragment();
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.parent, singleStringFragment);
+                    fragmentTransaction.commit();
+                    Log.d("TEST", "Run");
+                }
+            });
+        }
+
+    }
+}
---
+++ sample/src/main/java/powroznik/christopher/sample/SingleStringFragment.java
@@ -0,0 +1,76 @@
+package powroznik.christopher.sample;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import powroznik.christopher.save_to_activity.SaveToActivity;
+
+public class SingleStringFragment extends Fragment {
+
+    SaveToActivity saveToActivity = new SaveToActivity();
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.single_string_fragment, container, false);
+        final EditText putKey = (EditText) rootView.findViewById(R.id.putKey);
+        final EditText putString = (EditText) rootView.findViewById(R.id.putString);
+        final EditText getKey = (EditText) rootView.findViewById(R.id.getKey);
+        final TextView getString = (TextView) rootView.findViewById(R.id.getValue);
+        Button putButton = (Button) rootView.findViewById(R.id.put);
+        Button getButton = (Button) rootView.findViewById(R.id.get);
+        Button closeFragment = (Button) rootView.findViewById(R.id.closeFragment);
+
+        //put
+        if (putButton != null && putKey != null && putString != null) {
+            putButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String key = putKey.getText().toString();
+                    String string = putString.getText().toString();
+                    if (!key.isEmpty() && !string.isEmpty()) {
+                        try {
+                            saveToActivity.PutSingleString(getActivity(), key, string);
+                        } catch (ClassNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+        }
+
+        //get
+        if (getButton != null && getKey != null && getString != null) {
+            getButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String key = getKey.getText().toString();
+                    if (!key.isEmpty()) {
+                        try {
+                            getString.setText(saveToActivity.GetSingleHashString(getActivity(), key));
+                        } catch (ClassNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+        }
+
+        //Close Fragment
+        if (closeFragment != null) {
+            closeFragment.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    getActivity().getSupportFragmentManager().beginTransaction().remove(SingleStringFragment.this).commit();
+                }
+            });
+        }
+
+        return rootView;
+    }
+}
